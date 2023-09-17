66df82385cfec94ef290f747a9661b1b5c68e9b4
Brian <brianpark1101@gmail.com>
2023-09-17 02:21:04 -0400

    need to find solutions to: - what if there is already a reservation - no bookNowCards available, should it refresh until a time 6:05 AM? added: - implicit wait for user profile visibility after login button clicks - *new* made new method "getClosestCard in Booking.java to return the closest card to the specified time" - CompletePurchase.java - Terms.java

--- app/src/main/java/com/autoBooker/App.java
+++ app/src/main/java/com/autoBooker/App.java
@@ -2,18 +2,11 @@ package com.autoBooker;
 
 import com.autoBooker.component.BookNowCard;
 import com.autoBooker.page.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.text.SimpleDateFormat;
-import java.time.Duration;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import static java.lang.Math.abs;
@@ -36,30 +29,8 @@ public class App {
         Calendar nextWeek = Calendar.getInstance();
         nextWeek.add(Calendar.DAY_OF_WEEK, 7);
         bookingPage.navTo(GolfCourse.ThamesClassic, nextWeek, endHr, Golfers.FOUR, Holes.EIGHTEEN, startHr);
-        List<BookNowCard> availableCards = bookingPage.getAvailableCards();
+        bookingPage.getClosestCard().bookNow();
 
-        BookNowCard closestCard = null;
-        // the card may become unavailable if someone books it and produces an error if that happens when trying to add BookNowCard
-        while (true) {
-            try {
-                int closestDiff = Integer.MAX_VALUE;
-                for (BookNowCard card : availableCards) {
-                    int tenAM = 10 * 60; // 10 AM in minutes
-                    int difference = abs(tenAM - card.getTimeAsMin());
-                    if (difference < closestDiff) {
-                        closestDiff = difference;
-                        closestCard = card;
-                    }
-                }
-                if (closestCard != null) {
-                    closestCard.bookNow();
-                }
-                break;
-            }
-            catch (NoSuchElementException e) {
-                availableCards = bookingPage.getAvailableCards();
-            }
-        }
         BookingOptions bookingOptions = new BookingOptions(driver);
         bookingOptions.clickGolfers(Golfers.FOUR);
         bookingOptions.clickCheckout();
--- app/src/main/java/com/autoBooker/page/Booking.java
+++ app/src/main/java/com/autoBooker/page/Booking.java
@@ -2,16 +2,22 @@ package com.autoBooker.page;
 
 import com.autoBooker.ElementHandler;
 import com.autoBooker.component.BookNowCard;
+import net.bytebuddy.build.Plugin;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static java.lang.Math.abs;
 
 public class Booking extends ElementHandler {
 
@@ -40,9 +46,34 @@ public class Booking extends ElementHandler {
         List<WebElement> timeList = getElements(cardTimeBy);
 
         int cardCount = countElements(cardButtonBy); // gets cardCount using countElements from ElementHandler
-        for(int id = 0; id < cardCount; id++) {
+        for (int id = 0; id < cardCount; id++) {
             cards.add(new BookNowCard(driver, buttonList, timeList, id)); // searches for card with bookNowButton and adds
         }
         return cards;
+    }
+    public BookNowCard getClosestCard() {
+        List<BookNowCard> availableCards = getAvailableCards();
+
+        BookNowCard closestCard = null;
+        // the card may become unavailable if someone books it and produces an error if that happens when trying to add BookNowCard
+        while (true) {
+            try {
+                int closestDiff = Integer.MAX_VALUE;
+                for (BookNowCard card : availableCards) {
+                    int tenAM = 10 * 60; // 10 AM in minutes
+                    int difference = abs(tenAM - card.getTimeAsMin());
+                    if (difference < closestDiff) {
+                        closestDiff = difference;
+                        closestCard = card;
+                    }
+                }
+                break;
+            }
+            catch (Exception e) { // not sure which exception type to use to catch the specific exception
+                driver.navigate().refresh();
+                availableCards = getAvailableCards();
+            }
+        }
+        return closestCard;
     }
 }
